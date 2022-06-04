13ef8d8c297885169fc550f9a0293e51a56328bd
zacharybyre <zsbyre@gmail.com>
2022-06-04 00:59:11 -0600

    first commit

---
+++ python_string_interpolation.py
@@ -0,0 +1,14 @@
+name = 'Kristine'
+product = 'Python elearning course'
+
+email_content = f'''
+Hi {name}
+
+Thank you for purchasing {product}
+
+Regards,
+
+Sales Team
+'''
+
+print(email_content)
